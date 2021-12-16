5228a8117d427a436f4bfb3f0a4e345588dc9915
juliergo <julie_rago@hotmail.fr>
2021-12-16 16:12:26 +0100

    Ecriture test moyenne 💕

--- tests_funcs.py
+++ tests_funcs.py
@@ -9,5 +9,16 @@ class TestFuncs(unittest.TestCase):
 		self.assertEqual(funcs.max_int(-1,2),2)
 		self.assertEqual(funcs.max_int(0,0),0)
 
+	def test_moyenne_int(self):
+		self.assertEqual(funcs.max_int(0,2),1)
+		self.assertEqual(funcs.max_int(-1,-5),-3)
+		self.assertEqual(funcs.max_int(-1,3),1)
+		self.assertEqual(funcs.max_int(0,0),0)
+
 if __name__ == '__main__':
 	unittest.main()
+
+
+#git push -u origin moyenne
+#git checkout main
+#git merge moyenne
