c84cf3278f3abc25d25f18c87419b667722cd398
juliergo <julie_rago@hotmail.fr>
2021-12-30 23:55:14 +0100

    Ajout fonction is_geometric

--- funcs.py
+++ funcs.py
@@ -41,4 +41,14 @@ def ecart_type_int(list):
 	return st_dev
 
 def is_geometric(li):
-	return -1
+	if (li == []):
+		return None
+
+	if len(li) <= 1:
+		return True
+
+	ratio = li[1]/float(li[0])
+	for i in range(1, len(li)):
+		if (li[i]/float(li[i-1])) != ratio:
+			return False
+	return True
