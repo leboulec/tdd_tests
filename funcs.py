13abcfabdd73fac7da5e0c3f0c215acf05821b87
Clément Leboulenger <clement.leboulenger@pm.me>
2022-01-04 12:14:28 +0100

    Ajout fonction add_user

--- funcs.py
+++ funcs.py
@@ -1,3 +1,6 @@
+import re
+import sqlite3
+
 def max_int(a,b):
 	if a < b :
 		return b
@@ -24,4 +27,31 @@ def mediane_int(l):
 		return tmp[len(tmp) // 2]
 
 def add_user(username, password, spublickey, sprivatekey, epublickey, eprivatekey):
-    pass
+	# Check password
+	reg = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!#%*?&]{6,20}$"
+	pat = re.compile(reg)
+	mat = re.search(pat, password)
+	if not mat:
+		return None
+	
+	# Check key lenght
+	if len(spublickey) != 128 or len(sprivatekey) != 128 or len(epublickey) != 128 or len(eprivatekey) != 128:
+		return None
+	
+	# Check unique username
+	conn = sqlite3.connect('test.db')
+	conn.row_factory = sqlite3.Row
+	c = conn.cursor()
+
+	query = "SELECT * FROM users WHERE username='" + username + "'"
+	c.execute(query)
+	rows = c.fetchall()
+	if len(rows) != 0:
+		return None
+
+	c.execute("INSERT INTO users (username, password, spublickey, sprivatekey, epublickey, eprivatekey) VALUES(?,?,?,?,?,?)", (username, password, spublickey, sprivatekey, epublickey, eprivatekey))
+
+	conn.commit()
+	conn.close()
+		
+	return username
